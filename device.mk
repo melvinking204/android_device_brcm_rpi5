c96640c38d8cdc3b0d7e4c9bce7d831c012b94db
Melvin King <mking@starlightunlimited.net>
2026-07-03 11:49:49 +0000

    rpi5: Include vendor/astra/astra.mk for custom boot animation and customizations

--- device.mk
+++ device.mk
@@ -188,3 +188,4 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+-include vendor/astra/astra.mk
