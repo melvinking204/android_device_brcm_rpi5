6b4c1c6ad87cf5954b2186f6e9b86234fd41478e
Melvin King <mking@starlightunlimited.net>
2026-07-06 14:34:18 +0000

    rpi5: Rebrand device identifiers to AstraMail

--- aosp_rpi5.mk
+++ aosp_rpi5.mk
@@ -49,6 +49,7 @@ PRODUCT_COPY_FILES += \
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := rpi5
 PRODUCT_NAME := aosp_rpi5
-PRODUCT_BRAND := Raspberry
-PRODUCT_MODEL := Pi 5
-PRODUCT_MANUFACTURER := Raspberry
+PRODUCT_BRAND := AstraMail
+PRODUCT_MODEL := AstraMail
+PRODUCT_MANUFACTURER := Starlight Unlimited
+
